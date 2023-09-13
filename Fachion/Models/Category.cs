67a714e846de0fd26c7955d7308254a278478f31
Benzeer <Benzeer@LAPTOP-HD4L2JBS>
2023-09-13 17:34:16 +0530

    Add project files.

---
+++ Fachion/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fachion.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int Cid { get; set; }
+        [Required]
+        public string Cname { get; set; }
+    }
+}
---
+++ Fachion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fachion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
