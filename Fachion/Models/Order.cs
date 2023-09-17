36ad0259b58d0de039a4d444beeb5052fe556ab0
Benzeer <Benzeer@LAPTOP-HD4L2JBS>
2023-09-17 13:20:46 +0530

    update

--- Fachion/Models/Order.cs
+++ Fachion/Models/Order.cs
@@ -4,14 +4,14 @@ namespace Fachion.Models
 {
     public class Order
     {
-       
+     
         public int OrderID { get; set; }
         public int CustomerID { get; set; }
         public DateTime OrderDate { get; set; }
-        [NotMapped]
         public DateTime? ShipDate { get; set; }
         [NotMapped]
-        public decimal TotalAmount { get; set; }
+        
+       public decimal TotalAmount { get; set; }
         [NotMapped]
         public string PaymentMethod { get; set; }
         [NotMapped]
---
+++ Fachion/Models/OrderCrud.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.SqlClient;
+
+namespace Fachion.Models
+{
+    public class OrderCrud
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        IConfiguration configuration;
+        public OrderCrud(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            con = new SqlConnection(this.configuration.GetConnectionString("defaultConnection"));
+        }
+
+
+        public int AddOrder(Order order)
+        {
+
+            int result = 0;
+            string qry = "insert into Order values(@o_date,@cus_id)";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@o_date", order.OrderDate);
+            cmd.Parameters.AddWithValue("@cus_id", order.CustomerID);
+            
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+
+        
+        public int DeleteOrder(Order order)
+        {
+            int result = 0;
+            string qry = "delete order where o_id=@o_id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@o_id", order.OrderID);
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+       
+
+
+
+
+    }
+}
